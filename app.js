236cbff6bc8fad447f531cb2da995198e84e43dc
yuwa shrestha <yuwashrestha2004@gmail.com>
2026-01-27 22:44:17 +0545

    built a landing page with a little routing

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'landingpage.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
